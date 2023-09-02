4037c891e80c7456147791f8d21983a586c48e9a
Munkh-Erdene12 <munkherdene.n1207@gmail.com>
2023-09-02 15:49:47 +0800

    Initial version

--- src/app/about/page.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from "react";
-
-export default function page() {
-  const [state, setState] = React.useState("sainu");
-  return <div>{state}</div>;
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,5 @@
 import React from "react";
 import Link from "next/link";
 export default function page() {
-  return (
-    <div>
-      <p>home huudas</p>
-      <Link href="about">about huudas</Link>
-    </div>
-  );
+  return <div>home</div>;
 }
